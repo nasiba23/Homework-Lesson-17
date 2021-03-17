765c1302aba7a9b1447f88f9c4f8bfd5960017c7
Nasiba <nasyrova.nasiba@gmail.com>
2021-03-17 17:22:53 +0500

    finished 1st task

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace HomeworkLesson17
 {
@@ -6,7 +7,24 @@ namespace HomeworkLesson17
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            FirstTask();
+        }
+
+        static void FirstTask()
+        {
+            Console.Write("Enter a word: ");
+            var word = Console.ReadLine();
+            var newWord = word
+                .ToCharArray()
+                .Select(ch => ch == 'a' ? '1' : ch)
+                .Select(ch => ch == 'e' ? '2' : ch)
+                .Select(ch => ch == 'i' ? '3' : ch)
+                .Select(ch => ch == 'o' ? '4' : ch)
+                .Select(ch => ch == 'u' ? '5' : ch);
+            foreach (var x in newWord)
+            {
+                Console.Write(x);
+            }
         }
     }
 }
